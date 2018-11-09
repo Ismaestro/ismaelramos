2da6835955ba021ee9720058aa46dd8f08920e9d
Ismael Ramos <ramossilvanismael@gmail.com>
2018-11-09 13:45:48 +0100

    Adds i18n

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
 import {AppComponent} from './app.component';
+import localeEs from '@angular/common/locales/es';
+import {registerLocaleData} from '@angular/common';
+import localeEsExtra from '@angular/common/locales/extra/es';
+
+registerLocaleData(localeEs, 'es-ES', localeEsExtra);
 
 @NgModule({
   declarations: [
